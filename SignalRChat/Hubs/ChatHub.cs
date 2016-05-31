9d1083c7b406d9c7bb3d2f37f3d8d7d5363580e8
Joey Fox <joefoxman@gmail.com>
2016-05-31 16:26:09 -0400

    Added code to join chat rooms

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
 using Microsoft.AspNet.SignalR;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR.Hubs;
@@ -8,7 +10,8 @@ namespace SignalRChat
     [HubName("Chat")]
     public class ChatHub : Hub
     {
-        
+        public static ConcurrentDictionary<string, UserData> UserList = new ConcurrentDictionary<string, UserData>();
+
         public void Send(string name, string message)
         {
             // Call the addNewMessageToPage method to update clients.
@@ -16,17 +19,27 @@ namespace SignalRChat
             Clients.All.addNewMessageToPage(name, msg);
         }
 
-        public void JoinRoom(string room)
-        {
+        public void JoinRoom(string name, string userToChatWith, string room) {
             //NOTE: This is not persisted.
             Groups.Add(Context.ConnectionId, room);
+            var key = name + room;
+            UserList.TryAdd(key, new UserData {RoomId = room, Connected = true, ConnectionId = Context.ConnectionId, UserName = name});
+            var users = userToChatWith.Split(';');
+            foreach (var user in users) {
+                key = user + room;
+                UserList.TryAdd(key, new UserData { RoomId = room, Connected = true, ConnectionId = "", UserName = user });
+            }
         }
 
-        public void SendMessageToRoom(string room, string message)
+        public void SendMessageToRoom(string name, string room, string message)
         {
-
             var msg = String.Format("{0} : {1}", Context.ConnectionId, message);
-            Clients.Group(room).newMessage(msg);
+            // broadcast all users in this room
+            var usersInRoom = UserList.Where(a => a.Value.RoomId.Equals(room, StringComparison.OrdinalIgnoreCase));
+            var returnUserList = usersInRoom.Aggregate("", (current, user) => current + ";" + user.Value.UserName);
+            Clients.All.isUserInRoom(returnUserList.Substring(1), room);
+
+            Clients.Group(room).newMessage(name, msg);
         }
 
         public override Task OnConnected()
@@ -36,5 +49,14 @@ namespace SignalRChat
 
             return base.OnConnected();
         }
+    }
+
+    public class UserData
+    {
+        public string ConnectionId { get; set; }
+        public bool Connected { get; set; }
+        public string Ip { get; set; }
+        public string UserName { get; set; }
+        public string RoomId { get; set; }
     }
 }
