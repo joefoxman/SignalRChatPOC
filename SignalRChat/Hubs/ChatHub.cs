27e0b71873758b3220a1642041eb9ec500375079
Joey Fox <joefoxman@gmail.com>
2016-05-31 17:10:40 -0400

    Latest code changes to ensure a user does not get notified more than needed to join a chat room

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -19,11 +19,26 @@ namespace SignalRChat
             Clients.All.addNewMessageToPage(name, msg);
         }
 
-        public void JoinRoom(string name, string userToChatWith, string room) {
-            //NOTE: This is not persisted.
+        public void JoinRoom(string name, string room)
+        {
             Groups.Add(Context.ConnectionId, room);
             var key = name + room;
+            UserList.TryAdd(key, new UserData { RoomId = room, Connected = true, ConnectionId = Context.ConnectionId, UserName = name });
+        }
+
+        public void JoinRoom(string name, string userToChatWith, string room) {
+            Groups.Add(Context.ConnectionId, room);
+            var key = name + room;
+
+            // if already keyed in this list just update the ConnectionId because it could be an empty string
+            var userInList = UserList.FirstOrDefault(a => a.Key.Equals(key));
+            if (userInList.Value != null && string.IsNullOrWhiteSpace(userInList.Value.ConnectionId)) {
+                userInList.Value.ConnectionId = Context.ConnectionId;
+            }
+            // user who initiated room
             UserList.TryAdd(key, new UserData {RoomId = room, Connected = true, ConnectionId = Context.ConnectionId, UserName = name});
+
+            // other users to be in the chat room
             var users = userToChatWith.Split(';');
             foreach (var user in users) {
                 key = user + room;
@@ -35,11 +50,12 @@ namespace SignalRChat
         {
             var msg = String.Format("{0} : {1}", Context.ConnectionId, message);
             // broadcast all users in this room
-            var usersInRoom = UserList.Where(a => a.Value.RoomId.Equals(room, StringComparison.OrdinalIgnoreCase));
+            var usersInRoom = UserList.Where(a => a.Value.RoomId.Equals(room, StringComparison.OrdinalIgnoreCase) && string.IsNullOrWhiteSpace(a.Value.ConnectionId));
             var returnUserList = usersInRoom.Aggregate("", (current, user) => current + ";" + user.Value.UserName);
-            Clients.All.isUserInRoom(returnUserList.Substring(1), room);
 
-            Clients.Group(room).newMessage(name, msg);
+            Clients.All.isUserInRoom(returnUserList.Substring(1), room, name, msg);
+
+            Clients.Group(room).newMessage(name, msg, room);
         }
 
         public override Task OnConnected()
